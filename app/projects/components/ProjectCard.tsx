61908d9fdeed51643445e84b38c1f59e73335a42
Fang Jin <windmaomao@gmail.com>
2024-07-25 19:01:41 -0400

    add more projects and description

--- app/projects/components/ProjectCard.tsx
+++ app/projects/components/ProjectCard.tsx
@@ -1,10 +1,5 @@
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  Button,
-} from "@nextui-org/react";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
 
 import { type Project } from "./project";
 
@@ -13,47 +8,49 @@ interface ProjectCardProps {
 }
 
 export default function ProjectCard({ project }: ProjectCardProps) {
-  const { type, title, technologies } = project;
+  const { type, title, technologies, image, features, role } = project;
 
   return (
     <div>
-      <Card isPressable className="py-4">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">{type}</p>
-          <small className="text-default-500">{technologies}</small>
-          <h4 className="font-bold text-large">{title}</h4>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <Popover placement="top">
-            <PopoverTrigger>
+      <Popover placement="top">
+        <PopoverTrigger>
+          <Card isPressable className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <p className="text-tiny uppercase font-bold">{type}</p>
+              <h4 className="font-bold text-large">{title}</h4>
+              <small className="text-default-500">
+                Stack: <span className="font-bold">{technologies}</span>
+              </small>
+              <small className="text-default-500">
+                Roles: <span className="font-bold">{role}</span>
+              </small>
+            </CardHeader>
+            <CardBody className="overflow-visible py-2">
               <Image
                 alt="Card background"
                 className="object-cover rounded-xl"
-                src="https://nextui.org/images/hero-card-complete.jpeg"
+                height={135}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                src={
+                  image || "https://nextui.org/images/hero-card-complete.jpeg"
+                }
+                style={{ objectFit: "fill" }}
                 width={270}
               />
-            </PopoverTrigger>
-            <PopoverContent>
-              <div className="px-2 py-4 w-56">
-                <div className="text-small font-bold mb-2">Features</div>
-                <div className="text-tiny">
-                  <ul className="list-disc">
-                    <li>eCommerce amazon buy-now checkout experience</li>
-                    <li>React/Typescript/NextJS with micro-frontend design</li>
-                    <li>
-                      Use server side component to enhance page performance
-                    </li>
-                    <li>
-                      Token/theme system for dynamic style adjustments at
-                      runtime
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </CardBody>
-      </Card>
+            </CardBody>
+          </Card>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-4 py-4 w-56">
+            <div className="text-small font-bold mb-2">Features</div>
+            <div className="text-tiny ">
+              <ul className="list-disc h-20 overflow-y-scroll">
+                {features?.map((feature, i) => <li key={i}>{feature}</li>)}
+              </ul>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
--- app/projects/components/project.ts
+++ app/projects/components/project.ts
@@ -1,24 +1,43 @@
 export interface Project {
   type: string;
   title: string;
+  image?: string;
   description: string;
   date: string;
   technologies?: string;
+  features?: string[];
+  role?: string;
 }
 
 export const projectsMock: Project[] = [
   {
     type: "eCommerce",
     title: "Pearson",
+    image: "/pearson.jpg",
     description: "xxx",
-    date: "",
+    date: "Mar 2023 - May 2024",
     technologies: "React, Typescript, NextJs",
+    features: [
+      "eCommerce amazon buy-now checkout experience",
+      "React/Typescript/NextJS with micro-frontend design",
+      "Use server side component to enhance page performance",
+      "Token/theme system for dynamic style adjustments at runtime",
+    ],
+    role: "Senior front-end",
   },
   {
     type: "Medical Device",
     title: "PhysIQ",
+    image: "/physiq.jpeg",
     description: "xxx",
     date: "",
     technologies: "React, Typescript, NextJs",
+    features: [
+      "Series B Health Tech Startup",
+      "Developed a clinical web portal with React Typescript)/NextJS for real-time medical device data consumption",
+      "Built a multi-tenant architecture with permission-controlled settings for tracking multiple subjects",
+      "Created reusable components with Storybook and ensured quality with Cypress testing",
+    ],
+    role: "Staff engineer",
   },
 ];
